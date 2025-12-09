f4272fb988b926d6ae651d3bef2dd0aaf1848d10
Alejandro Cerezo <alce65@hotmail.es>
2025-12-09 23:02:48 +0100

    Extract core componentes Header and Footer from App

---
+++ projects/demo1/src/core/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+export const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </footer>
+    );
+};
